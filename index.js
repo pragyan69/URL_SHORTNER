209953464e0fab373d744584733b3777c86339c9
pragyan69 <prajyanqw@gmail.com>
2024-02-23 09:13:22 +0530

    3rd try

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl'); // This will be your model
 const cors = require('cors')
+const cookieParser = require('cookie-parser');
+
+const { v4: uuidv4 } = require('uuid');
+function generateUniqueVisitorId() {
+    return uuidv4();
+}
+
+
 const app = express();
 app.use(cors({
     origin: '*' // This will allow requests from any origin
 }));
+
+app.use(cookieParser())
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-// MongoDB connection success event
 mongoose.connection.on('connected', () => {
     console.log('MongoDB database connection established successfully');
   });
-  
-  // MongoDB connection error event
   mongoose.connection.on('error', (err) => {
     console.error('MongoDB connection error:', err.message);
   });
@@ -32,31 +39,38 @@ app.post('/shorten', async (req, res) => {
   
   // Route to redirect to the original URL and track click
   app.get('/:shortUrl', async (req, res) => {
-    const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
-    if (!shortUrl) {
-      return res.sendStatus(404);
-    }
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const clickIndex = shortUrl.clicks.findIndex(click => click.ip === ip);
-  
-    if (clickIndex >= 0) {
-      shortUrl.clicks[clickIndex].count++;
+    const { shortUrl } = req.params;
+    const urlEntry = await ShortUrl.findOne({ shortUrl: shortUrl });
+
+    if (urlEntry) {
+        const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).split(",")[0];
+        let clickEntry = urlEntry.clicks.find(c => c.ip === ip);
+
+        if (clickEntry) {
+            clickEntry.count++;
+        } else {
+            urlEntry.clicks.push({ ip, count: 1 });
+        }
+
+        await urlEntry.save();
+        res.redirect(urlEntry.originalUrl);
     } else {
-      shortUrl.clicks.push({ ip, count: 1 });
+        res.status(404).send('Shortened URL not found');
     }
+});
+
   
-    await shortUrl.save();
-    res.redirect(shortUrl.originalUrl);
-  });
 
   // Route to get click statistics
-app.get('/clicks/:shortUrl', async (req, res) => {
+
+  app.get('/clicks/:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
     if (!shortUrl) {
       return res.sendStatus(404);
     }
     res.json(shortUrl.clicks);
   });
+
   
   
 app.listen(port, () => console.log(`Listening on port ${port}`));
